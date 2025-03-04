fcf8099fc1a505b89bd43f1a69dfd9f9b2079d69
ShalewaAbdulkareem <abdulkareemtemilayo@gmail.com>
2025-03-04 06:13:32 +0100

    i made changes to my products

--- petrosa_project/petrosa_app/views.py
+++ petrosa_project/petrosa_app/views.py
@@ -11,24 +11,6 @@ def services(request):
     return render(request, 'services.html')
 def products(request):
     return render(request, 'products.html')
-def diesel_generator(request):
-    return render(request, 'diesel_generator.html')
-def gas_generator(request):
-    return render(request, 'gas_generator.html')
-def electrical_panels(request):
-    return render(request, 'electrical_panels.html')
-def drives(request):
-    return render(request, 'drives.html')
-def kv_panels(request):
-    return render(request, 'kv_panels.html')
-def led_light(request):
-    return render(request, 'led_light.html')
-def spare_part(request):
-    return render(request,'spare_part.html')
-def transformer(request):
-    return render(request, 'transformer.html')
-def cables(request):
-    return render(request, 'cables.html')
 
 def blog(request):
     return render(request, 'blog.html')
